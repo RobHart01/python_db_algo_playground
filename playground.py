7f466159d16b18d32940ed40557ec9e58be494be
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-10-30 08:54:39 +0900

    learning for loops

--- playground.py
+++ playground.py
@@ -1,16 +1,41 @@
 # 1.
 print("Hello World")
+print("--------------------")
 
 # 2.
 msg = "Hello World"
 print(msg)
+print("--------------------")
 
 # 3.
 num1 = 20
 num2 = 2
 sumnum = num1 + num2
 print(sumnum)
+print("--------------------")
 
 # 4.
 hobbies = []
 print(len(hobbies))
+print("--------------------")
+
+# 5.
+hobbies.append("Coding")
+print(hobbies)
+print(hobbies[0])
+
+hobbies.append('Photography')
+print(hobbies)
+print(hobbies[1])
+
+hobbies.append("Real Estate")
+print(hobbies)
+print(len(hobbies))
+print("--------------------")
+
+# 6. For loops
+print("Here are my hobbies:")
+for one_hobby in hobbies:
+  print(one_hobby)
+
+
