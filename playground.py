4636c5006666f9c18d395014fdc1fb2dc70a27db
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-10-30 09:05:25 +0900

    working more on list

--- playground.py
+++ playground.py
@@ -19,23 +19,46 @@ hobbies = []
 print(len(hobbies))
 print("--------------------")
 
-# 5.
+# 5. Lists
 hobbies.append("Coding")
 print(hobbies)
 print(hobbies[0])
+
+print("--------------------")
 
 hobbies.append('Photography')
 print(hobbies)
 print(hobbies[1])
 
+print("--------------------")
+
 hobbies.append("Real Estate")
 print(hobbies)
 print(len(hobbies))
+
+print("--------------------")
+
+hobbies.pop(1)
+print(hobbies)
+
+print("--------------------")
+
+hobbies.append("Dancing")
+print(hobbies)
+hobbies.remove("Dancing")
+
 print("--------------------")
 
 # 6. For loops
 print("Here are my hobbies:")
 for one_hobby in hobbies:
   print(one_hobby)
+print("--------------------")
+x = 0
+for one_hobby in hobbies:
+  x = x + 1
+  print(x, one_hobby)
+print("--------------------")
+
 
 
