85d89a7aa1149f5d0c7cc2b37c83c4671b781fc5
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-03 22:06:12 +0900

    playing around with modules

---
+++ people.py
@@ -0,0 +1,20 @@
+person1 = {
+  "name": "Robert",
+  "age": 22,
+  "country": "U.S."
+}
+person2 = {
+  "name": "Cameron",
+  "age": 24,
+  "country": "U.S."
+}
+person3 = {
+  "name": "Alex",
+  "age": 21,
+  "country": "U.S."
+}
+person4 = {
+  "name": "Clifford",
+  "age": 19,
+  "country": "U.S."
+}
--- usingmodules.py
+++ usingmodules.py
@@ -1,3 +1,6 @@
 import modules
+import people
 
 modules.say_hi("Robert")
+z = people.person4["name"]
+a = modules.say_hi(z)
