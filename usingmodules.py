feb2781af74bf1f77911220fb3f1b0f3fb5c2ede
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-04 09:48:52 +0900

    Playing around with json

--- usingmodules.py
+++ usingmodules.py
@@ -1,6 +1,8 @@
 import modules
 import people as peep
 import platform as pf
+import datetime
+import json
 
 modules.say_hi("Robert")
 z = peep.person4["name"]
@@ -9,4 +11,15 @@ modules.say_hi(z)
 x = pf.system()
 print(x)
 y = dir(pf)
-print(y)git 
+print(y)
+
+print("----------")
+v = dir(datetime)
+print(v)
+print("----------")
+c = datetime.datetime.now()
+print(c)
+print(c.year)
+print(c.strftime("%A"))
+print(c.day)
+
