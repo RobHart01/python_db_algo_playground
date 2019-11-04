c8afcc53294abcf4312062ac9c3123407ad67c59
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-04 15:40:00 +0900

    connecting to pymongo

--- playground_beta.py
+++ playground_beta.py
@@ -4,3 +4,11 @@ print("Username is: " + username)
 price = 50
 txt = "The price is {} dollars"
 print(txt.format(price))
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Telsa", model = "Model Y"))
+
+age = 21
+name = "Alex"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
