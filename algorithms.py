22aa6b63b9311165a5a6819b5b71c363f7ea6502
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-05 08:44:18 +0900

    Palindrome

--- algorithms.py
+++ algorithms.py
@@ -6,3 +6,18 @@ y = x - y
 x = x - y
 print("After Swapping: x =", x, " y =", y)
 
+#2 Check if array/list is palindrome
+# Create a function
+def isPalindrome(arr):
+  # Create for loop with range() starting from 0, iterating through the length of array, then divide it by two.
+  for i in range(0, int(len(arr) / 2)):
+    # If arr[position i] doesn't equal array length substracted by 1, substracted by i.
+    if arr[i] != arr[len(arr) - 1 - i]:
+      # Then return false.
+      return False
+    # If the conditions don't apply, reply true.
+    return True
+# Create test array
+testarray = [1,2,3,4,4,3,2,1]
+# Print out array
+print(isPalindrome(testarray))
